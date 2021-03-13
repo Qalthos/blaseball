80018f8da7a0509aecccfac9e92ff682d1944403
Nathaniel Case <this.is@nathanielca.se>
2021-03-13 16:46:36 -0500

    Try to cache standings

--- party/site.py
+++ party/site.py
@@ -1,12 +1,15 @@
 from blaseball_mike import models
 from flask import Flask, render_template
+from flask_caching import Cache
 
 from party import season
 
 app = Flask(__name__)
+cache = Cache(app)
 
 
 @app.route("/")
+@cache.cached(timeout=900)
 def standings() -> str:
     sim_data = models.SimulationData.load()
     subleagues = season.get_subleagues(sim_data.league)

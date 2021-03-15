b4031b33779f10c57ffc2367803d794c02959e01
Nathaniel Case <this.is@nathanielca.se>
2021-03-15 16:45:52 -0400

    Optionally cache standings to local josn file to assist load times

--- party/site.py
+++ party/site.py
@@ -1,3 +1,5 @@
+import json
+from datetime import datetime
 from typing import List
 
 from blaseball_mike import models
@@ -14,10 +16,35 @@ cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
 @app.route("/")
 @cache.cached(timeout=60)
 def show_standings() -> str:
-    sim_data = models.SimulationData.load()
-    standings = get_standings(sim_data.league.id, sim_data.season, sim_data.day)
+    standings_json = show_standings_json()
+    if standings_json:
+        bundle = json.loads(standings_json)
+        bundle["updated"] = datetime.fromisoformat(bundle["updated"])
+        return render_template("standings.j2", **bundle)
+    else:
+        sim_data = models.SimulationData.load()
+        standings = get_standings(
+            sim_data.league.id,
+            sim_data.season,
+            sim_data.day,
+        )
 
-    return render_template("standings.j2", sim=sim_data, standings=standings)
+        return render_template(
+            "standings.j2",
+            league=sim_data.league.name,
+            season=sim_data.season,
+            standings=standings,
+            updated=datetime.now(),
+        )
+
+
+@app.route("/standings.json")
+def show_standings_json() -> str:
+    try:
+        with open("/tmp/standings.json") as json_file:
+            return json_file.read()
+    except FileNotFoundError:
+        return ""
 
 
 @cache.memoize()

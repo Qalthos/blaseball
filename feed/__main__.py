818a5e0206ad8b1f0cfa98c38bbddf649d46b8f5
Nathaniel Case <Qalthos@gmail.com>
2021-04-16 11:15:32 -0400

    Add a bunch of event types

--- feed/__main__.py
+++ feed/__main__.py
@@ -20,6 +20,8 @@ MOD = [
     "#0a78a3",
     "#639e47",
 ]
+TAROT = "#a16dc3"
+ITEM = "#6dc0ff"
 
 
 def player_feed(player: str, category: Optional[int]):
@@ -35,10 +37,10 @@ def global_feed(category: Optional[int]):
 
 
 def _do_feed(feed: list[dict]) -> Table:
-    table = Table()
+    table = Table(expand=True)
     table.add_column("Day")
     table.add_column("Description")
-    table.add_column("Changes")
+    table.add_column("Changes", max_width=20)
 
     print(feed[0])
     for entry in feed:
@@ -50,12 +52,21 @@ def _do_feed(feed: list[dict]) -> Table:
         if entry["type"] == 29:
             # Entity talking
             changes = ""
+        elif entry["type"] == 54:
+            # Incineration
+            changes = ""
         elif entry["type"] == 57:
             # Ballpark renovations
             changes = ""
         elif entry["type"] == 70:
             # Grind Rail trick
             changes = ""
+        elif entry["type"] == 81:
+            # Tarot reading
+            changes = Text(
+                " ".join(str(card + 1) for card in metadata["spread"]),
+                style=TAROT,
+            )
         elif entry["type"] == 106:
             # Modification added
             mod_type = MOD[metadata["type"]]
@@ -92,8 +103,10 @@ def _do_feed(feed: list[dict]) -> Table:
         elif entry["type"] == 114:
             # Exchange players within a team
             changes = (
-                f"{metadata['aPlayerName']}: {LOC[metadata['aLocation']]} -> {LOC[metadata['bLocation']]}\n"
-                f"{metadata['bPlayerName']}: {LOC[metadata['bLocation']]} -> {LOC[metadata['aLocation']]}"
+                f"{metadata['aPlayerName']}:\n"
+                f" {LOC[metadata['aLocation']]} -> {LOC[metadata['bLocation']]}\n"
+                f"{metadata['bPlayerName']}:\n"
+                f" {LOC[metadata['bLocation']]} -> {LOC[metadata['aLocation']]}"
             )
         elif entry["type"] == 115:
             # Move a player
@@ -102,25 +115,57 @@ def _do_feed(feed: list[dict]) -> Table:
                 f"{LOC[metadata['location']]} -> {metadata['receiveTeamName']} "
                 f"{LOC[metadata['receiveLocation']]}"
             )
+        elif entry["type"] == 116:
+            # Replace a player
+            changes = (
+                f"{metadata['teamName']} {LOC[metadata['location']]}: "
+                f"{metadata['outPlayerName']} -> {metadata['inPlayerName']} "
+            )
         elif entry["type"] == 117:
             # Player stars increased
             changes = Text.assemble(
-                f"{metadata['before'] * 5:0.1f} -> ",
-                (f"{metadata['after'] * 5:0.1f}", "green"),
+                f"{_to_stars(metadata['before'])} -> ",
+                (_to_stars(metadata["after"]), "green"),
             )
         elif entry["type"] == 118:
             # Player stars decreased
             changes = Text.assemble(
-                f"{metadata['before'] * 5:0.1f} -> ",
-                (f"{metadata['after'] * 5:0.1f}", "red"),
+                f"{_to_stars(metadata['before'])} -> ",
+                (_to_stars(metadata["after"]), "red"),
             )
+        elif entry["type"] == 125:
+            # Entering the Hall
+            changes = ""
         elif entry["type"] == 127:
-            # Item stats increasing
+            # Player item added
+            item = _item_stars(metadata["playerItemRatingAfter"])
+            player = _to_stars(metadata["playerRating"])
             changes = Text.assemble(
-                f"{metadata['itemName']}: ",
-                f"{metadata['playerItemRatingBefore'] * 5:0.1f} -> ",
-                (f"{metadata['playerItemRatingAfter'] * 5:0.1f}", "green"),
+                ("+", "green"),
+                f"{metadata['itemName']}:\n",
+                f"  {player} -> {player} + ",
+                item,
             )
+        elif entry["type"] == 128:
+            # Player item dropped
+            item = _item_stars(metadata["playerItemRatingAfter"])
+            player = _to_stars(metadata["playerRating"])
+            changes = Text.assemble(
+                ("-", "red"),
+                f"{metadata['itemName']}:\n",
+                f"  {player} + ",
+                item,
+                f" -> {player}",
+            )
+        elif entry["type"] == 131:
+            # Reverb lineup shuffle
+            changes = ""
+        elif entry["type"] == 132:
+            # Reverb rotation shuffle
+            changes = ""
+        elif entry["type"] == 137:
+            # New player hatching
+            changes = ""
         elif entry["type"] == 144:
             # Exchange modifications
             mod_type = MOD[metadata["type"]]
@@ -155,11 +200,48 @@ def _do_feed(feed: list[dict]) -> Table:
         elif entry["type"] == 153:
             # Team had a stat mass changed
             changes = ""
+        elif entry["type"] == 171:
+            # A dependent mod was removed due to its dependency being removed
+            changes = Text(f"{metadata['source']}:")
+            for mod in metadata["removes"]:
+                changes.append("\n  -")
+                changes.append(mod["mod"], style=MOD[mod["type"]])
+        elif entry["type"] == 172:
+            # Dependent mods were added by another mod
+            changes = Text(f"{metadata['source']}:")
+            for mod in metadata["adds"]:
+                changes.append("\n  +")
+                changes.append(mod["mod"], style=MOD[mod["type"]])
         elif entry["type"] == 180:
             # Player secret stats reduced?
             changes = Text.assemble(
                 f"{metadata['before']:0.2f} -> ",
                 (f"{metadata['after']:0.2f}", "red"),
+            )
+        elif entry["type"] == 185:
+            # Player item is destroyed
+            changes = Text.assemble(
+                f"Durability: {metadata['itemHealthBefore']} -> ",
+                (str(metadata["itemHealthAfter"]), "red"),
+                f"\n  {_to_stars(metadata['playerRating'])} + ",
+                _item_stars(metadata["playerItemRatingBefore"]),
+                f" -> {_to_stars(metadata['playerRating'])}",
+            )
+        elif entry["type"] == 186:
+            # Player item is damaged
+            changes = Text.assemble(
+                f"Durability: {metadata['itemHealthBefore']} -> ",
+                (str(metadata["itemHealthAfter"]), "red"),
+            )
+        elif entry["type"] == 187:
+            # Player item is repaired
+            changes = Text.assemble(
+                f"Durability: {metadata['itemHealthBefore']} -> ",
+                (str(metadata["itemHealthAfter"]), "green"),
+                f"\n  {_to_stars(metadata['playerRating'])} + ",
+                _item_stars(metadata["playerItemRatingBefore"]),
+                f" -> {_to_stars(metadata['playerRating'])} + ",
+                _item_stars(metadata["playerItemRatingAfter"]),
             )
 
         table.add_row(day, description, changes)
@@ -167,12 +249,22 @@ def _do_feed(feed: list[dict]) -> Table:
     return table
 
 
+def _to_stars(rating: float) -> str:
+    return f"{rating * 5:0.1f}"
+
+
+def _item_stars(rating: float) -> Text:
+    style = ITEM if rating >= 0 else "red"
+    return Text(_to_stars(rating), style=style)
+
+
 def main():
     parser = argparse.ArgumentParser(description="Show Blaseball Feeds")
-    parser.add_argument("--category", type=int, default=None)
+    parser.add_argument("-c", "--category", type=int, default=None)
+    parser.add_argument("-n", "--interval", type=int, default=60)
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("--team", type=str)
-    group.add_argument("--player", type=str)
+    group.add_argument("-t", "--team", type=str)
+    group.add_argument("-p", "--player", type=str)
 
     args = parser.parse_args()
     if args.player:
@@ -185,7 +277,7 @@ def main():
     with Live(func(), vertical_overflow="crop") as live:
         while True:
             try:
-                time.sleep(60)
+                time.sleep(args.interval)
             except KeyboardInterrupt:
                 break
 

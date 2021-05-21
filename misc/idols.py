4fea8f2c9c947f303719806d057f1d82a5d031ea
Nathaniel Case <this.is@nathanielca.se>
2021-05-20 21:53:16 -0400

    Switch idols to refresh automatically

--- misc/idols.py
+++ misc/idols.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
+import time
+
 import requests
-from rich.live import Live
-from rich.table import Table
 from blaseball_mike import database
+from rich.console import RenderGroup
+from rich.live import Live
+from rich.panel import Panel
+from rich.table import Table
 
 URL = "https://www.blaseball.com/api/getIdols"
 
 
-def get_idols() -> tuple[list[str]]:
+def get_idols() -> tuple[list[str], list[str]]:
     resp = requests.get(URL).json()
     idols = resp["idols"][:resp["data"]["strictlyConfidential"] + 1]
     non_idols = resp["idols"][resp["data"]["strictlyConfidential"] + 1:]
@@ -20,13 +24,26 @@ def get_idols() -> tuple[list[str]]:
 
 
 def main() -> None:
-    mvp, non_mvp = get_idols()
+    with Live() as live:
+        while True:
+            mvp, non_mvp = get_idols()
 
-    for i, player in enumerate(mvp):
-        print(f"{i + 1: 2.0f} {player}")
-    print(r"\/\/\/\/\/\/\/\/\/")
-    for j, player in enumerate(non_mvp):
-        print(f"{i + j + 2: 2.0f} {player}")
+            above = Table.grid(expand=True)
+            above.add_column(width=3)
+            above.add_column()
+            for i, player in enumerate(mvp):
+                above.add_row(f"{i + 1}", player)
+
+            noodle = r"\/\/\/\/\/\/\/\/\/"
+
+            below = Table.grid(expand=True)
+            below.add_column(width=3)
+            below.add_column()
+            for j, player in enumerate(non_mvp):
+                below.add_row(f"{i + j + 2}", player)
+
+            live.update(Panel(RenderGroup(above, noodle, below)))
+            time.sleep(30)
 
 
 if __name__ == "__main__":

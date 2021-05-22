81cec403494385813b3550635f83a57cc931c26f
Nathaniel Case <this.is@nathanielca.se>
2021-05-21 21:12:47 -0400

    Show idol changes and also link to player pages for funsies

--- misc/idols.py
+++ misc/idols.py
@@ -9,6 +9,7 @@ from rich.panel import Panel
 from rich.table import Table
 
 URL = "https://www.blaseball.com/api/getIdols"
+PLAYER_URL = "https://www.blaseball.com/player"
 
 
 def get_idols() -> tuple[list[str], list[str]]:
@@ -18,31 +19,47 @@ def get_idols() -> tuple[list[str], list[str]]:
 
     players = database.get_player(resp["idols"])
     return (
-        [players[player]["name"] for player in idols],
-        [players[player]["name"] for player in non_idols],
+        [f"[link={PLAYER_URL}/{player}]{players[player]['name']}[/link]" for player in idols],
+        [f"[link={PLAYER_URL}/{player}]{players[player]['name']}[/link]" for player in non_idols],
     )
 
 
+def format_row(index: int, player: str, previous: list[str]) -> tuple[str, str, str]:
+    last = ""
+    if player != previous[index]:
+        try:
+            last = f"[strike]{previous.index(player) + 1}"
+        except ValueError:
+            last = "->"
+        player = f"[yellow]{player}"
+
+    return (last, f"{index + 1}", player)
+
+
 def main() -> None:
+    previous: list[str] = [""] * 20
     with Live() as live:
         while True:
             mvp, non_mvp = get_idols()
 
             above = Table.grid(expand=True)
+            above.add_column(width=2)
             above.add_column(width=3)
             above.add_column()
             for i, player in enumerate(mvp):
-                above.add_row(f"{i + 1}", player)
+                above.add_row(*format_row(i, player, previous))
 
-            noodle = r"\/\/\/\/\/\/\/\/\/"
+            noodle = r"[yellow]\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/"
 
             below = Table.grid(expand=True)
+            below.add_column(width=2)
             below.add_column(width=3)
             below.add_column()
             for j, player in enumerate(non_mvp):
-                below.add_row(f"{i + j + 2}", player)
+                below.add_row(*format_row(i + j + 1, player, previous))
 
             live.update(Panel(RenderGroup(above, noodle, below)))
+            previous = mvp + non_mvp
             time.sleep(30)
 
 
